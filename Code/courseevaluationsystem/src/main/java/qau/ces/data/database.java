a664ffec1f9b33a4a565c6ce9ac80dddb561252a
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-12-29 22:58:55 +0500

    db connectivity updated

--- Code/courseevaluationsystem/src/main/java/qau/ces/data/database.java
+++ Code/courseevaluationsystem/src/main/java/qau/ces/data/database.java
@@ -11,7 +11,7 @@ public class database {
             
             Class.forName("com.mysql.jdbc.Driver");
             
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/studentdata", "root", "");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=devmbilal", "devmbilal", "63013");
             return connect;
         }catch(Exception e){e.printStackTrace();}
         return null;
